01425a5cb278d0d3d1f5a8c955c05494d203e562
Everest Terry <eltartpro5@gmail.com>
2026-08-13 12:17:04 -0500

    Initial Commit

---
+++ kindling 3/kindling_3App.swift
@@ -0,0 +1,17 @@
+//
+//  kindling_3App.swift
+//  kindling 3
+//
+//  Created by Everest Terry on 8/13/26.
+//
+
+import SwiftUI
+
+@main
+struct kindling_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
